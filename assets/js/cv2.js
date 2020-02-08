7bf98618cc6bd43aad7835fca60b3d87ba8ca278
Huibant Victorien <32259001+HUIBANTVictorien@users.noreply.github.com>
2020-02-08 12:18:47 +0100

    Add files via upload

---
+++ assets/js/cv2.js
@@ -0,0 +1,18 @@
+/*fonction fadeout image sur div
+
+$(function() {
+  $('#expériences img').mouseenter(function() {
+    $('#expériences img').delay(1600).fadeOut(800);
+  });
+});
+ $('#expériences img').mouseleave(function() {
+    $('#expériences img').fadeIn(1000);
+  });
+});
+
+$(function() {
+  $('#formation img').mouseenter(function() {
+    $('#formation img').delay(1600).fadeOut(800);
+  });
+});
+*/
